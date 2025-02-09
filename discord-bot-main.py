03899ad421a7d1bf4853ac90c766103fdac2496d
mainLorhd <martunlemon@gmail.com>
2025-02-09 23:52:40 +0400

    Added auto leave and Stop command

--- discord-bot-main.py
+++ discord-bot-main.py
@@ -2,6 +2,7 @@ from config import token
 import nacl
 import discord
 from discord.ext import commands
+import time
 
 TOKEN = token
 PREFIX = '#'
@@ -27,7 +28,29 @@ async def play(ctx, link: str = None):
     else:
         connected = ctx.author.voice
         await connected.channel.connect()
-        await ctx.send(channel)
         await ctx.send("Connected!")
+
+@bot.command()
+async def stop(ctx):
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if not voice_client and not voice_client.is_connected():
+        await ctx.send("I'm doesn't connected to any Voice Chat")
+        return
+    if voice_client and voice_client.is_connected():
+        await voice_client.disconnect()
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    voice_client = discord.utils.get(bot.voice_clients, guild=member.guild)
+    voice_client = discord.utils.get(bot.voice_clients)
+    if len(voice_client.channel.members) == 1:
+        time.sleep(10)
+        if len(voice_client.channel.members) == 1:
+            text_channel = voice_client.guild.system_channel  
+            await text_channel.send("I'm alone in Voice Chat for 10 seconds. Bye!")
+            await voice_client.disconnect()
+            print("I AM LEAVING")
+        
+            
 
 bot.run(TOKEN)
